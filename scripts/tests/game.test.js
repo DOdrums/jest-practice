4c553d458ecad28a4628374d09f525baadc9684f
DOdrums <dirkornee@hotmail.com>
2022-06-01 08:55:24 +0000

    add newgame and showscore function and test scripts

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game } = require("../game")
+const { game, newGame, showScore } = require("../game")
 
 beforeAll(() => {
     let fs = require("fs");
@@ -25,4 +25,30 @@ describe("game object contains correct keys", () => {
     test("choices key exists", () => {
         expect("choices" in game).toBe(true);
     });
+    test("choices contain correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    })
+})
+
+describe("newGame works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        playerMoves = ["button1"]
+        currentGame = ["welp"]
+        document.getElementById("score").innerText = "42";
+        newGame();
+    })
+    test("should set game score to zero", () => {
+        expect(game.score).toEqual(0);
+    })
+    test("should set playerMoves to empty array", () => {
+        expect(game.playerMoves).toEqual([]);
+    })
+    test("should set currentGame to empty array", () => {
+        expect(game.currentGame).toEqual([]);
+    })
+    test("should display 0 for the element with the id of score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
+    })
+    
 })
